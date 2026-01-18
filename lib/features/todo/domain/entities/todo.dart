9ad9cae4f4e27940a73a264c5ea3791a4fc17075
RAJENDRA SINGH BISHT <rajanvisa2001@gmail.com>
2026-01-18 13:20:15 +0530

    refactor(todos-domain): add copywith in todo model

--- lib/features/todo/domain/entities/todo.dart
+++ lib/features/todo/domain/entities/todo.dart
@@ -10,4 +10,18 @@ class Todo {
     required this.title,
     required this.completed,
   });
+
+  Todo copyWith({
+    int? userId,
+    int? id,
+    String? title,
+    bool? completed,
+  }) {
+    return Todo(
+      userId: userId ?? this.userId,
+      id: id ?? this.id,
+      title: title ?? this.title,
+      completed: completed ?? this.completed,
+    );
+  }
 }
