d8e913af6230c203deb1d10b22d88aa2684bec81
RAJENDRA SINGH BISHT <rajanvisa2001@gmail.com>
2026-01-18 14:43:32 +0530

    feat: add multi bloc provider

---
+++ lib/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/di/injection.dart';
+import 'features/todo/presentation/pages/todo_screen.dart';
+import 'features/todo/presentation/blocs/todo_bloc.dart';
+import 'features/todo/presentation/blocs/todo_event.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter BLoC Clean Architecture',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider<TodoBloc>(
+            create: (_) => getIt<TodoBloc>()..add(const LoadTodos()),
+          ),
+        ],
+        child: const TodoScreen(),
+      ),
+    );
+  }
+}
