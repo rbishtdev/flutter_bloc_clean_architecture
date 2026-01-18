07e946f8e4bf54fa6a4d89d608e1c4a324e0e95c
RAJENDRA SINGH BISHT <rajanvisa2001@gmail.com>
2026-01-18 13:35:02 +0530

    feat(todos-data): add remote data sources

---
+++ lib/features/todo/data/datasources/remote/todo_remote_datasource.dart
@@ -0,0 +1,26 @@
+import '../../../../../core/data/remote/api_client.dart';
+import '../../models/todo_model.dart';
+
+abstract class TodoRemoteDataSource {
+  Future<List<TodoModel>> getTodos();
+  Future<void> addTodo(TodoModel todo);
+}
+
+class TodoRemoteDataSourceImpl implements TodoRemoteDataSource {
+  final ApiClient api;
+  TodoRemoteDataSourceImpl(this.api);
+
+  @override
+  Future<List<TodoModel>> getTodos() {
+    return api.get(
+      '/todos',
+      parser: (d) =>
+          (d as List).map((e) => TodoModel.fromJson(e)).toList(),
+    );
+  }
+
+  @override
+  Future<void> addTodo(TodoModel todo) {
+    return api.post('/todos', body: todo.toJson());
+  }
+}
